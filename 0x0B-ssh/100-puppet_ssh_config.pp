a9adad53be1658d6ec76198153270b36ee885b70
Diegoacosta127 <diegoacosta127@gmail.com>
2022-04-19 08:33:46 -0700

    output fix

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,8 +1,8 @@
 # SSH client configuration
 file { '/etc/ssh/ssh_config':
-    ensure  => 'present',
-    mode    => '0744',
-    owner   => 'root',
-    group   => 'root',
-    content => 'Host * \n PasswordAuthentication yes\nIdentityFile ~/.ssh/school'
+  ensure  => 'present',
+  owner   => 'root',
+  group   => 'root',
+  mode    => '0744',
+  content => 'Host * \n PasswordAuthentication yes\n IdentityFile ~/.ssh/school'
 }
