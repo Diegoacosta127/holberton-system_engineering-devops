a3eed31dbc9e668ce5c73fe01485f4491bd9e8d0
Diegoacosta127 <diegoacosta127@gmail.com>
2022-05-16 20:11:12 +0000

    Luciana told me to put the if at the end

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -6,20 +6,23 @@ import requests
 from sys import argv
 
 
+todos = 'https://jsonplaceholder.typicode.com/todos?userId={}'\
+        .format(argv[1])
+users = 'https://jsonplaceholder.typicode.com/users/{}'.format(argv[1])
+done = 0
+tasks = 0
+ret = requests.get(todos).json()
+user = requests.get(users).json()
+for elem in ret:
+    if elem.get('completed') is True:
+        done += 1
+    tasks += 1
+print('Employee {} is done with tasks({}/{}):'
+      .format(user.get('name'), done, tasks))
+for elem in ret:
+    if elem.get('completed') is True:
+        print('\t {}'.format(elem.get('title')))
+
+
 if __name__ == "__main__":
-    todos = 'https://jsonplaceholder.typicode.com/todos?userId={}'\
-            .format(argv[1])
-    users = 'https://jsonplaceholder.typicode.com/users/{}'.format(argv[1])
-    done = 0
-    tasks = 0
-    ret = requests.get(todos).json()
-    user = requests.get(users).json()
-    for elem in ret:
-        if elem.get('completed') is True:
-            done += 1
-        tasks += 1
-    print('Employee {} is done with tasks({}/{}):'
-          .format(user.get('name'), done, tasks))
-    for elem in ret:
-        if elem.get('completed') is True:
-            print('\t {}'.format(elem.get('title')))
+    pass
