e4a7cd5bf81f7938b64f2d643e15ee539e0f6d89
Diegoacosta127 <diegoacosta127@gmail.com>
2022-05-16 19:51:32 +0000

    using global variables

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -4,19 +4,23 @@
 
 import requests
 from sys import argv
+
+
+todos = 'https://jsonplaceholder.typicode.com/todos?userId={}'.format(argv[1])
+users = 'https://jsonplaceholder.typicode.com/users/{}'.format(argv[1])
+
+
 if __name__ == "__main__":
     done = 0
     tasks = 0
-    ret = requests.get('https://jsonplaceholder.typicode.com/todos?userId={}'
-                       .format(argv[1])).json()
+    ret = requests.get(todos).json()
+    user = requests.get(users).json()
     for elem in ret:
         if elem.get('completed') is True:
             done += 1
         tasks += 1
-    users = requests.get('https://jsonplaceholder.typicode.com/users/{}'
-                         .format(argv[1])).json()
     print('Employee {} is done with tasks({}/{}):'
-          .format(users.get('name'), done, tasks))
+          .format(user.get('name'), done, tasks))
     for elem in ret:
         if elem.get('completed') is True:
             print('\t{}'.format(elem.get('title')))
