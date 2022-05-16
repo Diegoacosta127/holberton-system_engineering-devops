edda15f0bdb4d63fb02159c32d0d29191fa6c527
Diegoacosta127 <diegoacosta127@gmail.com>
2022-05-16 23:39:40 +0000

    first commit

---
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""Script that returns info about TODO list progress of an employee"""
+
+
+import json
+import requests
+from sys import argv
+
+
+if __name__ == "__main__":
+    todos = 'https://jsonplaceholder.typicode.com/todos'
+    users = 'https://jsonplaceholder.typicode.com/users/'
+    ret = requests.get(todos).json()
+    user = requests.get(users).json()
+    with open('todo_all_employees.json', 'w') as f:
+        final = {}
+        for item in user:
+            id_user = item.get('id')
+            my_list = []
+            for elem in ret:
+                my_list.append({'username': item.get('username'),
+                                'task': elem.get('title'),
+                                'completed': elem.get('completed')})
+            final[str(id_user)] = my_list
+        json.dump(final, f)
